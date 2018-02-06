db02dee75e0d5d1f5deb4aee24f89583ffafd385
michael-zhao <michael.xin.zhao@gmail.com>
2018-02-05 23:39:18 -0500

    everything

--- src/PhD.java
+++ src/PhD.java
@@ -1,5 +1,6 @@
-/** NetID: mxz4, tcc78. Time spent: 
- * An instance maintains info about the PhD of a person. */
+/** NetIds: mxz4, tcc78. Time spent: 4 hours, 30 minutes.
+ * An instance maintains info about the PhD of a person. 
+ * Checked Javadoc output -- OK */
 public class PhD {
 	private String name; // name of person with a PhD, String of length > 0
 	private int month; // month PhD was awarded, range 1..12, 1 means Jan, etc.
@@ -9,35 +10,153 @@ public class PhD {
 	private int advisees; // number of PhD advisees of person
 	
 	/** 
-	 * Precondition: n has at least 1 char and m is in 1..12
+	 * Constructor: an instance for a person with name n, PhD month m, PhD year 
+	 * y. Its advisors are unknown, and it has no advisees.
+	 * Precondition: n has at least 1 char and m is in 1..12.
 	 */
 	public PhD(String n, int m, int y) {
-		name = n;
-		month = m;
-		year = y;
+		assert n.length() > 0;
+		assert m >= 1 && m <= 12;
+		
+		this.name = n;
+		this.month = m;
+		this.year = y;
 	}
 	
+	/**
+	 * Return the name of this person.
+	 */
 	public String name() {
-		return name;
+		return this.name;
 	}
 	
+	/**
+	 * Return the month this person got their PhD.
+	 */
 	public int month() {
-		return month;
+		return this.month;
 	}
 	
+	/**
+	 * Return the year this person got their PhD.
+	 */
 	public int year() {
-		return year;
+		return this.year;
 	}
 	
+	/**
+	 * Return the first advisor of this PhD (null if unknown).
+	 */
 	public PhD advisor1() {
-		return advisor1;
+		return this.advisor1;
 	}
 	
+	/**
+	 * Return the second advisor of this PhD (null if unknown or non-existent).
+	 */
 	public PhD advisor2() {
-		return advisor2;
+		return this.advisor2;
 	}
 	
+	/**
+	 * Return the number of PhD advisees of this person.
+	 */
 	public int numAdvisees() {
-		return advisees;
+		return this.advisees;
+	}
+	
+	/**
+	 * Add p as the first advisor of this person.
+	 * Precondition: the first advisor is unknown and p is not null.
+	 */
+	public void setAdvisor1(PhD p) {
+		assert this.advisor1 == null;
+		assert p != null;
+		
+		this.advisor1 = p;
+		p.advisees++;
+	}
+	
+	/**
+	 * Add p as the second advisor of this person. 
+	 * Precondition: The first advisor (of this person) is known, the second 
+	 * advisor is unknown, p is not null, and p is different from the first 
+	 * advisor.
+	 */
+	public void setAdvisor2(PhD p) {
+		assert this.advisor1 != null;
+		assert this.advisor2 == null;
+		assert p != null;
+		assert p != this.advisor1;
+		
+		this.advisor2 = p;
+		p.advisees++;
+	}
+	
+	/**
+	 * Constructor: a PhD with name n, PhD month m, PhD year y, first advisor 
+	 * adv1, and no second advisor. 
+	 * Precondition: n has at least 1 char, m is in 1..12, and adv is not null.
+	 */
+	public PhD(String n, int m, int y, PhD adv1) {
+		assert n.length() > 0;
+		assert m >= 1 & m <= 12;
+		assert adv1 != null;
+		
+		this.name = n;
+		this.month = m;
+		this.year = y;
+		this.advisor1 = adv1;
+		adv1.advisees++;
+	}
+	
+	/**
+	 * Constructor: a PhD with name n, PhD month m, PhD year y, first advisor 
+	 * adv1, and second advisor adv2. 
+	 * Precondition: n has at least 1 char, m is in 1..12, adv1 and adv2 are 
+	 * not null, and adv1 and adv2 are different.
+	 */
+	public PhD(String n, int m, int y, PhD adv1, PhD adv2) {
+		assert n.length() > 0;
+		assert m >= 1 && m <= 12;
+		assert adv1 != null && adv2 != null;
+		assert adv1 != adv2;
+		
+		this.name = n;
+		this.month = m;
+		this.year = y;
+		this.advisor1 = adv1;
+		adv1.advisees++;
+		this.advisor2 = adv2;
+		adv2.advisees++;
+	}
+	
+	/** 
+	 * Return value of "this PhD has at least one advisee", i.e. true if this 
+	 * PhD has at least one advisee and false otherwise
+	 */
+	public boolean hasAdvisee() {
+		return this.advisees > 0;
+	}
+	
+	/**
+	 * Return value of "p is not null and this person got the PhD after p."
+	 */
+	public boolean gotAfter(PhD p) {
+		return p != null && (p.year < this.year || 
+				(p.year == this.year && p.month < this.month));
+	}
+	
+	/**
+	 * Return value of "this person and p are intellectual siblings." 
+	 * Precondition: p is not null.
+	 */
+	public boolean areSiblings(PhD p) {
+		assert p != null;
+		
+		return (this.advisor1 == p.advisor1 || this.advisor1 == p.advisor2 || 
+				this.advisor2 == p.advisor2 || this.advisor2 == p.advisor1) && 
+				this != p && (this.advisor1 != null || this.advisor2 != null ||
+				p.advisor1 != null || p.advisor2 != null);
 	}
 }
--- src/PhDTest.java
+++ src/PhDTest.java
@@ -2,11 +2,130 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class PhDTest {
+public class PhDTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testConstructor1() {
+		PhD doctor = new PhD("Bob", 3, 1979);
+		assertEquals("Bob",doctor.name());
+		assertEquals(3,doctor.month());
+		assertEquals(1979,doctor.year());
+		assertEquals(null, doctor.advisor1());
+		assertEquals(null,doctor.advisor2());
+		assertEquals(0,doctor.numAdvisees());
+		
+		/* testing assert statements in the first constructor */
+		assertThrows(AssertionError.class, () -> new PhD("",0,0));
+		assertThrows(AssertionError.class, () -> new PhD("Joe",15,0));
+		assertThrows(AssertionError.class, () -> new PhD("Bob", -3, 0));
+	}
+	
+	@Test
+	public void testSetAdvisors() {		
+		PhD doc = new PhD("Joe", 5, 1992);
+		PhD doc2 = new PhD("Dave", 1, 1952);
+		PhD newdoc = new PhD("Kyle", 12, 2013);
+		PhD newdoc2 = new PhD("Rob", 10, 2013);
+		
+		assertEquals(null,newdoc.advisor1());
+		assertEquals(null,newdoc.advisor2());
+		
+		newdoc.setAdvisor1(doc);
+		assertEquals(doc,newdoc.advisor1());
+		assertEquals(1,doc.numAdvisees());
+		
+		newdoc.setAdvisor2(doc2);
+		assertEquals(doc2,newdoc.advisor2());
+		assertEquals(1,doc2.numAdvisees());
+		
+		newdoc2.setAdvisor1(doc);
+		assertEquals(doc,newdoc.advisor1());
+		assertEquals(2,doc.numAdvisees());
+		
+		/* testing assert statements in the setter methods */
+		assertThrows(AssertionError.class, () -> newdoc.setAdvisor1(doc));
+		assertThrows(AssertionError.class, () -> newdoc.setAdvisor1(null));
+		
+		PhD test = new PhD("Alpha", 1, 2000);
+		test.setAdvisor1(doc);
+		assertThrows(AssertionError.class, () -> test.setAdvisor2(doc));
+		
+		assertThrows(AssertionError.class, () -> newdoc2.setAdvisor2(doc));
+		assertThrows(AssertionError.class, () -> newdoc2.setAdvisor2(null));
+	}
+	
+	@Test
+	public void testConstructor2and3() {
+		PhD doc = new PhD("Joe", 5, 1992);
+		PhD doc2 = new PhD("Dave", 1, 1952);
+
+		PhD docc = new PhD("Tom", 3, 2014, doc);
+		assertEquals("Tom", docc.name());
+		assertEquals(3,docc.month());
+		assertEquals(2014,docc.year());
+		assertEquals(doc, docc.advisor1());
+		assertEquals(null, docc.advisor2());
+		assertEquals(0,docc.numAdvisees());
+		assertEquals(1,doc.numAdvisees());
+		
+		PhD docc2 = new PhD("Steve", 4, 2015, doc, doc2);
+		assertEquals("Steve",docc2.name());
+		assertEquals(4,docc2.month());
+		assertEquals(2015,docc2.year());
+		assertEquals(doc,docc2.advisor1());
+		assertEquals(doc2,docc2.advisor2());
+		assertEquals(0,docc2.numAdvisees());
+		assertEquals(2,doc.numAdvisees());
+		assertEquals(1,doc2.numAdvisees());
+		
+		/* testing assert statements for second and third constructors */
+		assertThrows(AssertionError.class, () -> new PhD("", 1, 2018, doc));
+		assertThrows(AssertionError.class, () -> new PhD("Jon", 0, 2018, doc));
+		assertThrows(AssertionError.class, () -> new PhD("Bob", 1, 2018, null));
+		
+		assertThrows(AssertionError.class, () -> new PhD("", 5, 2020, doc2));
+		assertThrows(AssertionError.class, 
+				() -> new PhD("Rex", 21, 2020, doc, doc2));
+		assertThrows(AssertionError.class, 
+				() -> new PhD("Matt", 5, 2020, null, doc2));
+		assertThrows(AssertionError.class, () -> new PhD("Jerry", 5, 2020, doc,
+				null));
+		assertThrows(AssertionError.class, () -> new PhD("Sue", 5, 2020, doc,
+				doc));
+		assertThrows(AssertionError.class, () -> new PhD("Beta", 5, 2020, null,
+				null));
+	}
+	
+	@Test
+	public void testGroupD() {
+		PhD jul53 = new PhD("Ty", 7, 1953);
+		PhD sep59 = new PhD("James", 9, 1959);
+		PhD jan88 = new PhD("Jane", 1, 1988, sep59, jul53);
+		PhD may75 = new PhD("Matt", 5, 1975, jul53);
+		PhD may752 = new PhD("Mark", 5, 1975);
+		PhD aug88 = new PhD("Jay", 8, 1988);
+		
+		assertEquals(false,jan88.hasAdvisee());
+		assertEquals(false,may75.hasAdvisee());
+		assertEquals(true,jul53.hasAdvisee());
+		assertEquals(true,sep59.hasAdvisee());
+		
+		assertEquals(false,jul53.gotAfter(sep59));
+		assertEquals(true,jan88.gotAfter(may75));
+		assertEquals(false,sep59.gotAfter(jan88));
+		assertEquals(false,may75.gotAfter(may75));
+		assertEquals(false,may75.gotAfter(may752));
+		assertEquals(true, aug88.gotAfter(jan88));
+		
+		assertEquals(true,jan88.areSiblings(may75));
+		assertEquals(false,jul53.areSiblings(sep59));
+		assertEquals(false,jul53.areSiblings(jan88));
+		assertEquals(true,may75.areSiblings(jan88));
+		assertEquals(false,sep59.areSiblings(sep59));
+		
+		/* testing assert statements for the three functions in Group D */
+		assertThrows(AssertionError.class, () -> jul53.areSiblings(null));
+		assertThrows(AssertionError.class, () -> may75.areSiblings(null));
 	}
 
 }
